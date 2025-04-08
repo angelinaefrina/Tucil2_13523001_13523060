6218e8c72d477d3b953bcab49bcbfef0134816c1
angelinaefrina <efrinaprahastaputri@gmail.com>
2025-04-08 10:29:44 +0700

    fix: quadtree node

--- src/strukturdata/QuadTreeNode.java
+++ src/strukturdata/QuadTreeNode.java
@@ -57,97 +57,74 @@ public class QuadTreeNode {
         }
     }
 
+    public static void divideBlockRecursively(QuadTreeNode node, Matriks region, int minBlockSize) {
+        if (node.isDivideable(minBlockSize)) {
+            QuadTreeNode[] children = node.divideBlock(region, minBlockSize);
+            if (children != null) {
+                for (QuadTreeNode child : children) {
+                    divideBlockRecursively(child, region, minBlockSize);
+                }
+            }
+        }
+    }
+
     // WIP
-//     public Pixel[][] createImage() {
-//         if (this.isParent && q1 != null && q2 != null && q3 != null && q4 != null) {
-//             Pixel[][] image1 = q1.createImage();
-//             Pixel[][] image2 = q2.createImage();
-//             Pixel[][] image3 = q3.createImage();
-//             Pixel[][] image4 = q4.createImage();
+    public Matriks createImage() {
+        if (this.isParent && q1 != null && q2 != null && q3 != null && q4 != null) {
+            Matriks image1 = q1.createImage();
+            Matriks image2 = q2.createImage();
+            Matriks image3 = q3.createImage();
+            Matriks image4 = q4.createImage();
 
-//             Pixel[][] top = concatHorizontal(image1, image2);
-//             Pixel[][] bottom = concatHorizontal(image3, image4);
-//             return concatVertical(top, bottom);
-//         } else {
-//             return fillBlockWithMeanColor();
-//         }
-//     }
+            Matriks atas = Matriks.ConcatHorizontally(image1, image2);
+            Matriks bawah = Matriks.ConcatHorizontally(image3, image4);
+            return Matriks.ConcatVertically(atas, bawah);
+        } else {
+            return normalizeBlockColor();
+        }
+    }
 
-//     private Pixel[][] fillBlockWithMeanColor() {
-//         int totalR = 0, totalG = 0, totalB = 0;
-//         for (int i = 0; i < h; i++) {
-//             for (int j = 0; j < w; j++) {
-//                 totalR += block[i][j].r;
-//                 totalG += block[i][j].g;
-//                 totalB += block[i][j].b;
-//             }
-//         }
-//         int size = w * h;
-//         int meanR = totalR / size;
-//         int meanG = totalG / size;
-//         int meanB = totalB / size;
+    private Matriks normalizeBlockColor() {
+        int totalRed = 0, totalGreen = 0, totalBlue = 0;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                totalRed += this.block.mat[i][j].getRed();
+                totalGreen += this.block.mat[i][j].getGreen();
+                totalBlue += this.block.mat[i][j].getBlue();
+            }
+        }
+        int size = w * h;
+        int meanRed = totalRed / size;
+        int meanGreen = totalGreen / size;
+        int meanBlue = totalBlue / size;
 
-//         Pixel[][] filled = new Pixel[h][w];
-//         for (int i = 0; i < h; i++) {
-//             for (int j = 0; j < w; j++) {
-//                 filled[i][j] = new Pixel(meanR, meanG, meanB);
-//             }
-//         }
-//         return filled;
-//     }
+        Matriks N = new Matriks(h, w);
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                N.mat[i][j] = new Pixel(meanRed, meanGreen, meanBlue);
+            }
+        }
+        return N;
+    }
 
-//     private Pixel[][] concatHorizontal(Pixel[][] left, Pixel[][] right) {
-//         int height = left.length;
-//         int widthLeft = left[0].length;
-//         int widthRight = right[0].length;
-//         Pixel[][] result = new Pixel[height][widthLeft + widthRight];
-
-//         for (int i = 0; i < height; i++) {
-//             System.arraycopy(left[i], 0, result[i], 0, widthLeft);
-//             System.arraycopy(right[i], 0, result[i], widthLeft, widthRight);
-//         }
-//         return result;
-//     }
-
-//     private Pixel[][] concatVertical(Pixel[][] top, Pixel[][] bottom) {
-//         int heightTop = top.length;
-//         int heightBottom = bottom.length;
-//         int width = top[0].length;
-//         Pixel[][] result = new Pixel[heightTop + heightBottom][width];
-
-//         for (int i = 0; i < heightTop; i++) {
-//             result[i] = top[i];
-//         }
-//         for (int i = 0; i < heightBottom; i++) {
-//             result[heightTop + i] = bottom[i];
-//         }
-//         return result;
-//     }
-
-//     public void createImage(BufferedImage image) {
-//         fillImageFromQuadTree(image, this);
-//     }
-
-//     private void fillImageFromQuadTree(BufferedImage image, QuadTreeNode node) {
-//         if (node == null) return;
-
-//         if (node.isDivideable(1)) {
-//             QuadTreeNode[] children = node.divideBlock(node.block, 1);
-//             if (children != null) {
-//                 for (QuadTreeNode child : children) {
-//                     fillImageFromQuadTree(image, child);
-//                 }
-//             }
-//         } else {
-//             for (int i = 0; i < node.h; i++) {
-//                 for (int j = 0; j < node.w; j++) {
-//                     Pixel p = node.block[i][j];
-//                     int rgb = (p.r << 16) | (p.g << 8) | p.b;
-//                     image.setRGB(node.x + j, node.y + i, rgb);
-//                 }
-//             }
-//         }
-//     }
-// }
+    public int countNodes() {
+        if (!this.isParent) {
+            return 1;
+        } else {
+            return 1 + q1.countNodes() + q2.countNodes() + q3.countNodes() + q4.countNodes();
+        }
+    }
+    
+    public int getMaxDepth() {
+        if (!this.isParent) {
+            return this.depth;
+        } else {
+            int d1 = q1.getMaxDepth();
+            int d2 = q2.getMaxDepth();
+            int d3 = q3.getMaxDepth();
+            int d4 = q4.getMaxDepth();
+            return Math.max(Math.max(d1, d2), Math.max(d3, d4));
+        }
+    }
 
 }
