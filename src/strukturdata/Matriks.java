b8d8784d8c3e366776a9458f75ba00389302c077
angelinaefrina <efrinaprahastaputri@gmail.com>
2025-04-08 10:44:05 +0700

    fix: matriks

--- src/strukturdata/Matriks.java
+++ src/strukturdata/Matriks.java
@@ -1,4 +1,6 @@
 package strukturdata;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Matriks { 
@@ -74,5 +76,49 @@ public class Matriks {
     public static int BanyakElemenMatriks(Matriks M){
         return M.baris * M.kolom;
     }
+
+    // Menggabung dua buah matriks secara horizontal
+    public static Matriks ConcatHorizontally(Matriks M, Matriks N){
+        Matriks Out = new Matriks(M.baris, M.kolom+N.kolom); // matriks baru memiliki kolom sebanyak gabungan kolom kedua matriks
+        for (int i = 0; i < Out.baris; i++) {
+            for (int j = 0; j < Out.kolom; j++) {
+                if (j < M.kolom) {
+                    Out.mat[i][j] = M.mat[i][j];
+                } else {
+                    Out.mat[i][j] = N.mat[i][j - M.kolom];
+                }
+            }
+        }
+        return Out;
+    }
+
+    // Menggabung dua buah matriks secara horizontal
+    public static Matriks ConcatVertically(Matriks M, Matriks N) {
+        Matriks Out = new Matriks(M.baris + N.baris, M.kolom); // matriks baru memiliki baris sebanyak gabungan baris kedua matriks
+        for (int i = 0; i < Out.baris; i++) {
+            for (int j = 0; j < Out.kolom; j++) {
+                if (i < M.baris) {
+                    Out.mat[i][j] = M.mat[i][j];
+                } else {
+                    Out.mat[i][j] = N.mat[i - M.baris][j];
+                }
+            }
+        }
+        return Out;
+    }
+
+    public void saveMatrixToText(Matriks M, String filePath) {
+    try (FileWriter writer = new FileWriter(filePath)) {
+        for (Pixel[] row : M.mat) {
+            for (Pixel p : row) {
+                writer.write("(" + p.getRed() + "," + p.getGreen() + "," + p.getBlue() + ") ");
+            }
+            writer.write("\n");
+        }
+        System.out.println("Matrix saved to: " + filePath);
+    } catch (IOException e) {
+        System.out.println("Error saving matrix to file: " + e.getMessage());
+    }
+}
 
 }
